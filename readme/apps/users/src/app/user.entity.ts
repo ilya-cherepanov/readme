560440b09ddcbd08a21c8f3052f637da764cb770
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2022-11-30 15:38:53 +0700

    feat(users): add user entity

---
+++ readme/apps/users/src/app/user.entity.ts
@@ -0,0 +1,41 @@
+import { genSalt, hash, compare } from 'bcrypt';
+import { User } from '@readme/shared-types';
+import { SALT_ROUNDS } from './user.constants';
+
+
+export class UserEntity implements User {
+  _id?: string;
+  email: string;
+  password: string;
+  name: string;
+  avatar?: string;
+
+  constructor(user: User) {
+     this.fillEntity(user);
+  }
+
+  public toObject(): object {
+    return {...this};
+  }
+
+  public async setPassword(password: string): Promise<this> {
+    const salt = await genSalt(SALT_ROUNDS);
+    this.password = await hash(password, salt);
+    return this;
+  }
+
+  public async checkPassword(password: string): Promise<boolean> {
+    return compare(this.password, password);
+  }
+
+  public fillEntity(user: User) {
+    this._id = user._id;
+    this.name = user.name;
+    this.email = user.email;
+    this.password = user.password;
+
+    if (user.avatar) {
+      this.avatar = user.avatar;
+    }
+  }
+}
