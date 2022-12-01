c46e327e7866431a31d079943a4eb17d33e8633b
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2022-12-01 20:01:06 +0700

    feat(shared types): add comment interface

--- readme/libs/shared-types/src/index.ts
+++ readme/libs/shared-types/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/user.interface';
 export * from './lib/post-category.enum';
 export * from './lib/post.interface';
 export * from './lib/post-status.enum';
+export * from './lib/comment.interface';
