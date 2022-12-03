0ff3fc08637065e95c8281abe393b660f121f47e
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2022-12-03 18:37:26 +0700

    refactor(comments): rename AppService to CommentsService

--- readme/apps/comments/src/app/app.module.ts
+++ readme/apps/comments/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CommentsService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [CommentsService],
 })
 export class AppModule {}
--- readme/apps/comments/src/app/app.service.ts
+++ readme/apps/comments/src/app/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class CommentsService {
   getData(): { message: string } {
     return { message: 'Welcome to comments!' };
   }
