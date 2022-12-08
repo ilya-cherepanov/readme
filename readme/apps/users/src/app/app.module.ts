dd4e1c9c4c320e21e5e27dc86243cfc5730bc309
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2022-12-08 15:40:08 +0700

    feat(users): add config module

--- readme/apps/users/src/app/app.module.ts
+++ readme/apps/users/src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { ENV_FILE_PATH } from './user.constants';
+import databaseConfig from './config/database.config';
+
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig,]
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- readme/apps/users/src/app/user.constants.ts
+++ readme/apps/users/src/app/user.constants.ts
@@ -1,1 +1,2 @@
 export const SALT_ROUNDS = 10;
+export const ENV_FILE_PATH = 'environments/.users.env';
